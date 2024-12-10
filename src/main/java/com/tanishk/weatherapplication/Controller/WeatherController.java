80490f7720df51ec481a3bb724761d0d5a8905ea
Tanishk Prasad <tanishkprasad8055@gmail.com>
2024-12-10 21:47:23 +0530

    Done with Simple Backend Structure

--- src/main/java/com/tanishk/weatherapplication/Controller/WeatherController.java
+++ src/main/java/com/tanishk/weatherapplication/Controller/WeatherController.java
@@ -1,20 +1,21 @@
 package com.tanishk.weatherapplication.Controller;
 
+import com.tanishk.weatherapplication.Response.WeatherResponse;
 import com.tanishk.weatherapplication.Service.weatherService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/weather")
 public class WeatherController {
     private final weatherService weatherService;
 
     public WeatherController(weatherService weatherService){
         this.weatherService = weatherService;
     }
-    @RequestMapping("/current")
+    @GetMapping("/weather/current")
     public ResponseEntity<?> getCurrentWeather(@RequestParam String city){
         try {
             String weatherData = weatherService.getCurrentWeather(city);
@@ -23,5 +24,9 @@ public class WeatherController {
         catch (Exception e){
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
+    @GetMapping("/weather/parsed")
+    public WeatherResponse getParsedWeather(@RequestParam String city){
+        return weatherService.getParsedWeather(city);
     }
 }
